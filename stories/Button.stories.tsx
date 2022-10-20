48e660dab0a52f4391eb3969deca9573fa2bfa92
André Knob <dextra.aafonso@creditas.com>
2022-10-19 21:50:23 -0300

    feat: added button stories

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { Button } from "../src";
 
-storiesOf("Button", module).add("Default", () => (
-  <Button backgroundColor="#7159c1" color="#fff">
-    test
-  </Button>
-));
+storiesOf("Button", module)
+  .add("Default", () => <Button>Default</Button>)
+  .add("Outlined", () => <Button outlined>Outlined</Button>)
+  .add("Disabled", () => <Button disabled>Default</Button>);
