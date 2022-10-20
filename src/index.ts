878be5c27a93718efa61d70524d44f37aa375ed1
André Knob <dextra.aafonso@creditas.com>
2022-10-19 22:00:30 -0300

    feat: adding Modal component

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { Button } from "./components/Button";
 export { Separator } from "./components/Separator";
 export { CheckBox } from "./components/CheckBox";
+export { Input } from "./components/Input";
+export { Modal } from "./components/Modal";
