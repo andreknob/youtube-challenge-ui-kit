e3220639d50ebdbfd59f10740d99f92d4350ce97
André Knob <dextra.aafonso@creditas.com>
2022-10-19 22:02:15 -0300

    feat: adding Radio component

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { Separator } from "./components/Separator";
 export { CheckBox } from "./components/CheckBox";
 export { Input } from "./components/Input";
 export { Modal } from "./components/Modal";
+export { Radio } from "./components/Radio";
